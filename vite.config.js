7e854fd7496129208c7376ca81cef589ea6373ac
Mohammad Masood Alam <130masoodalam@gmail.com>
2024-09-01 16:09:42 +0530

    changed base to ./ in package.json

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
-import { defineConfig } from "vite";
-
 module.exports = {
   build: {
     outDir: './build',
     emptyOutDir: true, // also necessary
   }
 }
-
-export default defineConfig({
-  base: ''
-})
